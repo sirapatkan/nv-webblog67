788b1a4aac9c51274f549d7090fae99cc2368058
unknown <sirapatkan>
2024-07-22 22:54:44 +0700

    MVC

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,21 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+
+
+
 const app = express();
 
-app.get('/status', function(req, res){
-    console.log('------------------------------');
-    console.log(req);
-    console.log('------------------------------');
-    res.send('Hello nodejs server');
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
 
-app.get('/hello/:person', function (req,res) {
-    console.log('hello - ' + req.params.person)
-    res.send('sey hello with ' + req.params.person)
-})
+require('./routes')(app);
 
-let port = 8081
+
+
+
+
+ 
+let port = 8081;
 
 app.listen(port, function(){
     console.log('Server running on http://localhost:' + port)
---
+++ server/src/controllers/UserController.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+    index(req, res){
+        res.send('ดูข้อมูล user ทุกคน');
+    },
+    create(req, res){
+    res.send("สร้าง user "+ JSON.stringify(req.body.username));
+    },
+    
+    put(req, res){
+        res.send('แก้ไข user คนที่ '+ req.params.userId + ' ข้อมูลที่แก้ ' +
+            JSON.stringify(req.body));
+        },
+    
+    
+    remove(req, res){
+        res.send('ลบ user คนที่ '+ req.params.userId);
+     },
+
+     show(req, res){
+        res.send('ดูข้อมูล user ทุกคน');
+     }
+        
+    
+        }
+    
+    
